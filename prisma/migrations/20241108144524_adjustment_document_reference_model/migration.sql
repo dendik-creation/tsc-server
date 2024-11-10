5d77565cd00deafb5d94759ba9075a215aa6ef0c
dendik-creation <setiawandendik0205@gmail.com>
2024-11-10 19:58:41 +0700

    Fix : Switch Database Porvider Mysql to Psql

--- prisma/migrations/20241108144524_adjustment_document_reference_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `allowed_file_type` on the `documentreference` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `documentreference` DROP COLUMN `allowed_file_type`;
